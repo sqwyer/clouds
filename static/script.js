4ac9da50f69a37f0d7c8b08ee7dab5aabf972155
Sqwyer <sawyer.codes.stuff@gmail.com>
2022-02-24 10:32:44 -0600

    Forecast + make better for mobile + add weather condition

--- static/script.js
+++ static/script.js
@@ -1,20 +1,48 @@
+let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 async function get (val) {
     if(!val) val = document.getElementById('query').value;
     localStorage.setItem('last-lookup', val);
-    await fetch(`https://api.weatherapi.com/v1/current.json?key=2379467b9adb453ea0525259222402&q=${val}`)
+    await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2379467b9adb453ea0525259222402&q=${val}&days=2`)
         .then(res => res.json())
         .then(json => populate(json, null))
         .catch(err => populate(null, err))
 }
 
+function populateForecast(data) {
+    document.getElementById('forecast').style.display = 'initial';
+    for(let i = 0; i < data.forecast.forecastday.length; i++) {
+        let t = data.forecast.forecastday[i];
+        let k = i + 1;
+        let d = new Date(t.date);
+        document.getElementById(`fc_d${k}_ico`).setAttribute('src', 'https:' + t.day.condition.icon);
+        document.getElementById(`fc_d${k}_day`).innerHTML = week[d.getDay()+1]
+        document.getElementById(`fc_d${k}_high`).innerHTML = t.day.maxtemp_f + '˚';
+        document.getElementById(`fc_d${k}_low`).innerHTML = t.day.mintemp_f + '˚';
+        document.getElementById(`fc_d${k}_rain`).innerHTML = t.day.daily_chance_of_rain + '% Precip.';
+    }
+}
+
 function populate (data, err) {
-    if(err) {
-        alert('Error: ' + err);
+    if(err != null && err != undefined) {
+        document.getElementById('region').innerHTML = "We expierenced an error";
+        document.getElementById('country').innerHTML = "try again!";
+        document.getElementById('name').innerHTML = "Error!";
+        document.getElementById('temp').innerHTML = "??";
+        document.getElementById('icon').style.display = 'none';
+        document.getElementById('desc').style.display = 'none';
+        document.getElementById('forecast').style.display = 'none';
+        console.error(err);
     } else if(data) {
+        document.getElementById('icon').style.display = 'initial';
+        document.getElementById('desc').style.display = 'initial';
+        document.getElementById('icon').setAttribute('src', 'https:' + data.current.condition.icon);
         document.getElementById('region').innerHTML = data.location.region;
         document.getElementById('country').innerHTML = data.location.country;
         document.getElementById('name').innerHTML = data.location.name;
         document.getElementById('temp').innerHTML = data.current.temp_f;
+        document.getElementById('desc').innerHTML = "(" + data.current.condition.text + ")";
+        populateForecast(data);
     }
 }
 
